5ea141848ddfd7c566942b8eb82d506264e43e1e
katrina <0993262626g@gmail.com>
2018-04-30 21:39:44 +0300

    add hr

--- portalnews/comment.js
+++ portalnews/comment.js
@@ -1,15 +1,33 @@
+
+
 $(document).ready(function () {
+
 	$(document).on("click","#add-comment", function () {
 		var form = $("form[name='form']");
 		form.css("display","block");
 		$("#add-comment").replaceWith(form);
+
 	});
+	
+		
+
+	
+
 
 	$(document).on("click", "input[name='send']", function(){
 		var comment= $("textarea[name='field']").val ();
 		alert('Подождите, идет загрузка комментариев...')
-		$("<p>" + comment + "</p>").appendTo("#comments");
+		$("<p>" + comment + "<hr>" + "</p>").appendTo("#comments");
+
+		
 	})
+
+  
+		
+document.getElementById('comments').style.display='inline-block';
+//document.getElementById('comments').style.background='#fff';
+
+
 
 });
 
