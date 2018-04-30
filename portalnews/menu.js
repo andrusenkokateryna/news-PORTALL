bf724a74995bce96fcf489603e7ac40346663101
katrina <0993262626g@gmail.com>
2018-04-30 14:20:00 +0300

    modified menu

--- portalnews/menu.js
+++ portalnews/menu.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
       let drop = document.querySelector('#dropdown');
       let hide = document.querySelector('.hide');
       let body = document.getElementsByTagName('body')
+       var news=document.getElementById('news-content')
       drop.addEventListener('click',function(){
       	   hide.className = 'show';
       })
@@ -9,7 +10,13 @@ document.addEventListener('DOMContentLoaded', function () {
      body[0].addEventListener('click',function(event){
       	    if(event.target.classList[0] == 'content'){
       	    	hide.className = 'hide';
+              news.style.display='none';
+
       	    }
 
       })
+     drop.addEventListener('click',function(event){
+     
+      news.style.display='block';
+     })
      });
